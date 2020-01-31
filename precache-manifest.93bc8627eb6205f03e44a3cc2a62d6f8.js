122737236acb707a6daa7e2513d8502b21e5868a
Valeriy Grechukha <v.mrfriz@gmail.com>
2020-02-01 00:45:56 +0300

    Updates

---
+++ precache-manifest.93bc8627eb6205f03e44a3cc2a62d6f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aaf24f819622c2f26ccd23ab3485ff9",
+    "url": "/vkminiapp-mafia/index.html"
+  },
+  {
+    "revision": "1ee6401c83f381e07c84",
+    "url": "/vkminiapp-mafia/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "b9da5c38ffedffbdfecb",
+    "url": "/vkminiapp-mafia/static/css/main.a0f3c4aa.chunk.css"
+  },
+  {
+    "revision": "1ee6401c83f381e07c84",
+    "url": "/vkminiapp-mafia/static/js/2.abc348f4.chunk.js"
+  },
+  {
+    "revision": "b9da5c38ffedffbdfecb",
+    "url": "/vkminiapp-mafia/static/js/main.5fc50414.chunk.js"
+  },
+  {
+    "revision": "674d6c0015a412ef205e",
+    "url": "/vkminiapp-mafia/static/js/runtime-main.b5a58479.js"
+  }
+]);
